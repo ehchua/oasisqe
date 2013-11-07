e52fed6abbe379cb13b09d2fdbeaf4b698f9db20
colincoghill <colin@colincoghill.com>
2013-11-07 20:36:59 +1300

    Start working on models for Course and Topic. Not yet complete/functional.

---
+++ src/oasis/models/Course.py
@@ -0,0 +1,453 @@
+# -*- coding: utf-8 -*-
+
+# This code is under the GNU Affero General Public License
+# http://www.gnu.org/licenses/agpl-3.0.html
+
+""" Courses.py
+    Handle course related operations.
+"""
+from oasis.lib import Topics
+
+from logging import log, ERROR
+import datetime
+
+from oasis.models.Group import Group
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text
+from oasis import db
+
+# WARNING: name and title are stored in the database as: title, description
+
+
+class Course(db.Model):
+    """ A topic contains a collection of questions.
+    """
+    __tablename__ = "courses"
+
+#CREATE TABLE courses (
+#    "course" SERIAL PRIMARY KEY,
+#    "title" character varying(128) NOT NULL,
+#    "description" text,
+#    "owner" integer,
+#    "active" integer DEFAULT 1,
+#    "type" integer,
+#    "practice_visibility" character varying DEFAULT 'all'::character varying,
+#    "assess_visibility" character varying DEFAULT 'enrol'::character varying
+#);
+
+    course = Column(Integer, primary_key=True)
+    name = Column("title", String(128), unique=True)
+    title = Column("description", Text)
+    owner = Column(Integer, ForeignKey('users.id'))
+    active = Column(Integer, default=1)
+    type = Column(Integer)
+    practice_visibility = Column(String(50), default="all")
+    assess_visibility = Column(String(50), default="enrol")
+
+    def get_users(self):
+        """ Return a list of users in the course"""
+        allusers = []
+        for g_id, group in Group.active_by_course(self.id).iteritems():
+            allusers.append(group.members())
+        return allusers
+
+    def groups(self):
+        """ Return a dict of groups currently attached to this course."""
+
+        return Group.active_by_course(self.id)
+
+    @staticmethod
+    def all(only_active=True):
+        """ Return a list of all courses in the system."""
+
+        if only_active:
+            return Course.query.filter_by(active = True)
+        return Course.query.order_by("name").all()
+
+    @staticmethod
+    def all_dict():
+        """ Return a summary of all courses, keyed by course id
+        """
+
+        courses = Course.all()
+        coursedict = {}
+        for course in courses:
+            coursedict[course.id] = course
+
+        return coursedict
+
+    @staticmethod
+    def get_by_name(name):
+        """ Return a course object for the given name, or None
+        """
+
+        return Course.query.filter_by(name=name).first()
+
+    @staticmethod
+    def get(course_id):
+        """ Return a course object for the given name, or None
+        """
+
+        return Course.query.filter_by(course=course_id).first()
+
+    @staticmethod
+    def create(name, title, owner, coursetype):
+        """ Add a course to the database."""
+
+        newc = Course()
+        newc.name = name
+        newc.title = title
+        newc.owner = owner
+        newc.type = coursetype
+
+        db.session.add(newc)
+        db.session.commit()
+        return newc
+
+    @staticmethod
+    def add_group(group_id, course_id):
+        """ Add a group to a course."""
+
+        db.engine.execute("INSERT INTO groupcourses (groupid, course) VALUES (%s, %s);", (group_id, course_id))
+
+    @staticmethod
+    def del_group(group_id, course_id):
+        """ Remove a group from the course."""
+
+        db.engine.execute("DELETE FROM groupcourses WHERE groupid=%s AND course=%s;", (group_id, course_id))
+
+    def get_topics_all(self, archived=2, numq=True):
+        """ Return a summary of all topics in the course.
+            if archived=0, only return non archived courses
+            if archived=1, only return archived courses
+            if archived=2, return all courses
+            if numq is true then include the number of questions in the topic
+        """
+    ret = None
+    if archived == 0:
+        ret = run_sql("""SELECT topic, title, position, visibility, archived
+                         FROM topics
+                         WHERE course=%s
+                           AND (archived='0' OR archived IS NULL)
+                         ORDER BY position, topic;""", (course,))
+    elif archived == 1:
+        ret = run_sql("""SELECT topic, title, position, visibility, archived
+                         FROM topics
+                         WHERE course=%s
+                           AND archived='1'
+                         ORDER BY position, topic;""", (course,))
+    elif archived == 2:
+        ret = run_sql("""SELECT topic, title, position, visibility, 0
+                         FROM topics
+                         WHERE course=%s
+                         ORDER BY position, topic;""", (course,))
+    info = {}
+    if ret:
+        count = 0
+        for row in ret:
+            info[count] = {'id': int(row[0]),
+                           'title': row[1],
+                           'position': row[2],
+                           'visibility': row[3],
+                           'archived': row[4]}
+            if info[count]['position'] is None or info[count]['position'] is "None":
+                info[count]['position'] = 0
+            if numq:
+                info[count]['numquestions'] = Topics.get_num_qs(int(row[0]))
+            count += 1
+    else:  # we probably don't have the archived flag in the Db yet
+        ret = run_sql(
+            """SELECT topic, title, position, visibility
+               FROM topics
+               WHERE course=%s
+               ORDER BY position, topic;""", (course,))
+        if ret:
+            count = 0
+            for row in ret:
+                info[count] = {'id': int(row[0]),
+                               'title': row[1],
+                               'position': row[2],
+                               'visibility': row[3]}
+                if info[count]['position'] is None or info[count]['position'] is "None":
+                    info[count]['position'] = 0
+                if numq:
+                    info[count]['numquestions'] = Topics.get_num_qs(int(row[0]))
+                count += 1
+    return info
+
+
+def get_topics(cid):
+    """ Return a list of all topics in the course."""
+    key = "course-%s-topics" % cid
+    obj = MC.get(key)
+    if obj:
+        return obj
+    sql = "SELECT topic FROM topics WHERE course=%s ORDER BY position;"
+    params = (cid,)
+    ret = run_sql(sql, params)
+    if ret:
+        topics = [row[0] for row in ret]
+        MC.set(key, topics)
+        return topics
+    MC.set(key, [])
+    return []
+
+
+def get_exams(cid, prev_years=False):
+    """ Return a list of all assessments in the course."""
+    assert isinstance(cid, int)
+    assert isinstance(prev_years, bool)
+    if not prev_years:
+        now = datetime.datetime.now()
+        year = now.year
+        sql = """SELECT exam
+                 FROM exams
+                 WHERE course=%s
+                   AND archived='0'
+                   AND "end" > '%s-01-01';"""
+        params = (cid, year)
+    else:
+        sql = """SELECT exam FROM exams WHERE course=%s;"""
+        params = (cid,)
+    ret = run_sql(sql, params)
+    if ret:
+        exams = [int(row[0]) for row in ret]
+        return exams
+    return []
+
+
+def _create_config_demonstration(course_id, period_id):
+    """ Create any needed groups/configs for a demonstration course
+    """
+
+    course = get_course(course_id)
+    # An ad-hoc Staff group
+    name = "C_%s_STAFF_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group()
+
+    group.name = name
+    group.title = "%s, Staff" % (course['name'],)
+    group.gtype = 1  # staff
+    group.source = "adhoc"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+
+    db.session.add(group)
+    db.session.commit()
+
+    add_group(group.id, course_id)
+
+    # An Open Registration student group
+    name = "C_%s_OPEN_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group()
+
+    group.name = name
+    group.title = "%s, Students, Self Registered" % (course['name'],)
+    group.gtype = 2  # enrolment
+    group.source = "open"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+
+    db.session.add(group)
+    db.session.commit()
+
+    add_group(group.id, course_id)
+
+    # An ad-hoc student group
+    name = "C_%s_ADHOC_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group()
+
+    group.name = name
+    group.title = "%s, Students" % (course['name'],)
+    group.gtype = 2  # student
+    group.source = "adhoc"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+    db.session.add(group)
+    db.session.commit()
+
+    add_group(group.id, course_id)
+
+
+def _create_config_casual(course_id, period_id):
+    """ Create any needed groups/configs for a casual course
+    """
+
+    course = get_course(course_id)
+    # An ad-hoc Staff group
+    name = "C_%s_STAFF_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group.get(g_id=0)
+
+    group.name = name
+    group.title = "%s, Staff" % (course['name'],)
+    group.gtype = 1  # staff
+    group.source = "adhoc"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+
+    db.session.add(group)
+    db.session.commit(group)
+
+    add_group(group.id, course_id)
+
+    # An ad-hoc student group
+    name = "C_%s_ADHOC_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group()
+
+    group.name = name
+    group.title = "%s, Students" % (course['name'],)
+    group.gtype = 2  # student
+    group.source = "adhoc"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+
+    db.session.add(group)
+    db.session.commit(group)
+
+    add_group(group.id, course_id)
+
+
+def _create_config_standard(course_id, period_id):
+    """ Create any needed groups/configs for a standard course
+    """
+
+    # standard
+    #    Create an adhoc staff group
+    #           COURSE_name_STAFF_period
+    #    Create a student group set to ad-hoc
+    #           COURSE_name_ADHOC_period
+    #    Create a student group set to (unconfigured) feed
+    #           COURSE_name_feed_period
+
+    course = get_course(course_id)
+    # An ad-hoc Staff group
+    name = "C_%s_STAFF_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group()
+
+    group.name = name
+    group.title = "%s, Staff" % (course['name'],)
+    group.gtype = 1  # staff
+    group.source = "adhoc"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+
+    db.session.add(group)
+    db.session.commit()
+
+    add_group(group.id, course_id)
+
+    # An ad-hoc student group
+    name = "C_%s_ADHOC_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group()
+
+    group.name = name
+    group.title = "%s, Students" % (course['name'],)
+    group.gtype = 2  # student
+    group.source = "adhoc"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+
+    db.session.add(group)
+    db.session.commit()
+
+    add_group(group.id, course_id)
+
+
+def _create_config_large(course_id, period_id):
+    """ Create any needed groups/configs for a large course
+    """
+
+    # large
+    #    Create a staff group set to (unconfigured) feed
+    #           COURSE_name_STAFF_feed_period
+    #    Create an adhoc staff group
+    #           COURSE_name_STAFF_period
+    #    Create a student group set to ad-hoc
+    #           COURSE_name_ADHOC_period
+    #    Create a student group set to (unconfigured) feed
+    #           COURSE_name_feed_period
+
+    course = get_course(course_id)
+    # An ad-hoc Staff group
+    name = "C_%s_STAFF_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group()
+
+    group.name = name
+    group.title = "%s, Staff" % (course['name'],)
+    group.gtype = 1  # staff
+    group.source = "adhoc"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+
+    db.session.add(group)
+    db.session.commit()
+
+    add_group(group.id, course_id)
+
+    # An ad-hoc student group
+    name = "C_%s_STAFF_%s" % (course['name'], period_id)
+    group = Group.get_by_name(name)
+    if not group:
+        group = Group()
+
+    group.name = name
+    group.title = "%s, Students" % (course['name'],)
+    group.gtype = 2  # student
+    group.source = "adhoc"
+    group.period = period_id
+    group.feed = None
+    group.feedargs = ""
+    group.active = True
+
+    db.session.add(group)
+    db.session.commit()
+
+    add_group(group.id, course_id)
+
+
+def create_config(course_id, coursetemplate, period_id):
+    """ Course is being created. Setup some configuration depending on
+        given values.
+    """
+
+    # First, course template
+    if coursetemplate == "demo":
+        _create_config_demonstration(course_id, period_id)
+    elif coursetemplate == "casual":
+        _create_config_casual(course_id, period_id)
+    elif coursetemplate == "standard":
+        _create_config_standard(course_id, period_id)
+    elif coursetemplate == "large":
+        _create_config_large(course_id, period_id)
+
---
+++ src/oasis/models/Topic.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+
+# This code is under the GNU Affero General Public License
+# http://www.gnu.org/licenses/agpl-3.0.html
+
+""" Topic.py
+    Handle topic related operations.
+"""
+import json
+
+from logging import log, ERROR, INFO
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from oasis import db
+
+
+
+class Topic(db.Model):
+    """ A topic contains a collection of questions.
+    """
+    __tablename__ = "topics"
+#
+#CREATE TABLE topics (
+#    "topic" SERIAL PRIMARY KEY,
+#    "course" integer REFERENCES courses("course") NOT NULL,
+#    "title" character varying(128) NOT NULL,
+#    "visibility" integer,
+#    "position" integer DEFAULT 1,
+#    "archived" boolean DEFAULT false
+#);
+
+    topic = Column(Integer, primary_key=True)
+    course = Column(Integer, )
+
+
+
+def create(course_id, name, vis, pos=1):
+    """Add a topic to the database."""
+    key = "course-%s-topics" % course_id
+    MC.delete(key)
+    log(INFO,
+        "db/Topics/create(%s, %s, %s, %s)" % (course_id, name, vis, pos))
+    conn = dbpool.begin()
+    conn.run_sql("""INSERT INTO topics (course, title, visibility, position)
+                    VALUES (%s, %s, %s, %s);""",
+                    (course_id, name, vis, pos))
+    res = conn.run_sql("SELECT currval('topics_topic_seq');")
+    dbpool.commit(conn)
+    if res:
+        return res[0][0]
+    log(ERROR,
+        "Topic create error (%s, %s, %s, %s)" % (course_id, name, vis, pos))
+    return 0
+
+
+def get_topic(topic_id):
+    """ Fetch a dictionary of topic values"""
+    key = "topic-%s-record" % topic_id
+    obj = MC.get(key)
+    if obj:
+        return json.loads(obj)
+    sql = """SELECT topic, course, title, visibility, position, archived
+             FROM topics
+             WHERE topic=%s;"""
+    params = (topic_id,)
+    ret = run_sql(sql, params)
+    if not ret:
+        raise KeyError("Unable to find topic %s" % topic_id)
+    row = ret[0]
+    topic = {
+        'id': row[0],
+        'course': row[1],
+        'title': row[2],
+        'visibility': row[3],
+        'position': row[4],
+        'archived': row[5]
+    }
+    if topic['position'] is None or topic['position'] is "None":
+        topic['position'] = 0
+    MC.set(key, json.dumps(topic))
+    return topic
+
+
+def get_name(topic_id):
+    """Fetch the name of a topic."""
+    return get_topic(topic_id)['title']
+
+
+def set_name(topic_id, name):
+    """Set the name of a topic."""
+    assert isinstance(topic_id, int)
+    assert isinstance(name, str) or isinstance(name, unicode)
+    run_sql("UPDATE topics SET title=%s WHERE topic=%s;", (name, topic_id))
+    key = "topic-%s-record" % topic_id
+    MC.delete(key)
+
+
+def get_pos(topic_id):
+    """Fetch the position of a topic."""
+    assert isinstance(topic_id, int)
+    return get_topic(topic_id)['position']
+
+
+def set_pos(topic_id, pos):
+    """Update the position of a topic."""
+    if pos is None or pos is "None":
+        pos = 0
+    run_sql("""UPDATE topics
+               SET position=%s
+               WHERE topic=%s;""", (pos, topic_id))
+    key = "topic-%s-record" % topic_id
+    MC.delete(key)
+    course = get_course_id(topic_id)
+    key = "course-%s-topics" % course
+    MC.delete(key)
+
+
+def get_course_id(topic_id):
+    """Fetch the course ID of the course a topic belongs to"""
+    return get_topic(topic_id)['course']
+
+
+def get_vis(topic_id):
+    """Fetch the visibility of a topic."""
+    return get_topic(topic_id)['visibility']
+
+
+def set_vis(topic_id, vis):
+    """Update the visibility of a topic."""
+    run_sql("""UPDATE topics SET visibility=%s WHERE topic=%s;""",
+            (vis, topic_id))
+    key = "topic-%s-record" % topic_id
+    MC.delete(key)
+
+
+def get_num_qs(topic_id):
+    """Tell us how many questions are in the given topic."""
+    key = "topic-%s-numquestions" % topic_id
+    obj = MC.get(key)
+    if obj:
+        return int(obj)
+    sql = """SELECT count(topic)
+            FROM questiontopics
+            WHERE topic=%s
+             AND position > 0;
+            """
+    params = (topic_id,)
+    try:
+        res = run_sql(sql, params)
+        if not res:
+            num = 0
+        else:
+            num = int(res[0][0])
+        MC.set(key, num, 180)  # 3 minute cache
+        return num
+    except LookupError:
+        raise IOError("Database connection failed")
+
+
+def get_qts(topic_id):
+    """ Return a dictionary of the QTemplates in the given Topic, keyed by qtid.
+        qtemplates[qtid] = {'id', 'position', 'owner', 'name', 'description',
+                            'marker', 'maxscore', 'version', 'status'}
+    """
+    sql = """select qtemplates.qtemplate, questiontopics.position,
+                qtemplates.owner, qtemplates.title, qtemplates.description,
+                qtemplates.marker, qtemplates.scoremax, qtemplates.version,
+                qtemplates.status
+            from questiontopics,qtemplates
+            where questiontopics.topic=%s
+            and questiontopics.qtemplate = qtemplates.qtemplate;"""
+    params = (topic_id,)
+    ret = run_sql(sql, params)
+    qtemplates = {}
+    if ret:
+        for row in ret:
+            qtid = int(row[0])
+            pos = int(row[1])
+            owner = int(row[2])
+            name = row[3]
+            desc = row[4]
+            marker = row[5]
+            scoremax = row[6]
+            version = int(row[7])
+            status = row[8]
+            qtemplates[qtid] = {'id': qtid,
+                                'position': pos,
+                                'owner': owner,
+                                'name': name,
+                                'description': desc,
+                                'marker': marker,
+                                'maxscore': scoremax,
+                                'version': version,
+                                'status': status}
+    return qtemplates
